5ac5d4e3e4154449c4c053e1ad2b2eb474e6ae2c
chalpenny <cp.halpenny@gmail.com>
2020-02-13 22:10:47 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const headMain = document.querySelector('main');
+
+headMain.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Colleen is the champion'
+document.body.appendChild(newHeader);
